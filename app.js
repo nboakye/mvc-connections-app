4121e2ad3808c5d1c6edd48e78b2d4c6de1a6b5d
Nanayaw Boakye <nboakye@uncc.edu>
2021-11-04 22:00:29 -0400

    Started connection to mongo database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const { MongoClient, Db } = require('mongodb');
 const connectionRoutes = require('./routes/connectionRoutes');
 const mainRoutes = require('./routes/mainRoutes');
 
@@ -11,6 +12,7 @@ const app = express();
 // Configure the application
 let port = 3000;
 let host = 'localhost';
+let url = 'mongodb://localhost:27017';
 app.set('view engine', 'ejs');
 
 // Use middleware functions
@@ -18,6 +20,29 @@ app.use(express.static('public'));    // Be able to serve static files, looks in
 app.use(express.urlencoded({extended: true}));        // Parse data in request body for PUT requests
 app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
+
+// connect to database
+/* MongoClient.connect(url, (err, client) => {
+    if(err) {
+        console.log(err.message);
+    } else {
+        db = client.db('4166app');
+        // Start server
+        app.listen(port, host, () => {
+            console.log('Server is running on port', port);
+        });
+    }
+}); */
+
+MongoClient.connect(url)
+.then(client => {
+    db = client.db('4166app');
+    // Start server
+    app.listen(port, host, () => {
+        console.log('Server is running on port', port);
+    });
+})
+.catch(err => console.log(err.message));
 
 // Set up routes
 app.use('/', mainRoutes);
@@ -40,7 +65,3 @@ app.use((err, req, res, next) => {
     res.render('error', {error: err});
 });
 
-// Start server
-app.listen(port, host, () => {
-    console.log('Server is running on port', port);
-});
