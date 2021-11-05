38b2e64d09e967065d6a82120e0ac752d5dee76c
Nanayaw Boakye <nboakye@uncc.edu>
2021-11-04 22:17:03 -0400

    Started connection to mongodb using mongoose

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
-const { MongoClient, Db } = require('mongodb');
+const mongoose = require('mongoose');
 const connectionRoutes = require('./routes/connectionRoutes');
 const mainRoutes = require('./routes/mainRoutes');
+const {initCollection} = require('./models/connection');
 
 // Create application
 const app = express();
@@ -15,34 +16,20 @@ let host = 'localhost';
 let url = 'mongodb://localhost:27017';
 app.set('view engine', 'ejs');
 
-// Use middleware functions
-app.use(express.static('public'));    // Be able to serve static files, looks in public folder
-app.use(express.urlencoded({extended: true}));        // Parse data in request body for PUT requests
-app.use(morgan('tiny'));
-app.use(methodOverride('_method'));
-
-// connect to database
-/* MongoClient.connect(url, (err, client) => {
-    if(err) {
-        console.log(err.message);
-    } else {
-        db = client.db('4166app');
-        // Start server
-        app.listen(port, host, () => {
-            console.log('Server is running on port', port);
-        });
-    }
-}); */
-
-MongoClient.connect(url)
-.then(client => {
-    db = client.db('4166app');
-    // Start server
+// connect to the database
+mongoose.connect('mongodb://localhost:27017/4166app')
+.then(()=> {
     app.listen(port, host, () => {
         console.log('Server is running on port', port);
     });
 })
 .catch(err => console.log(err.message));
+
+// Use middleware functions
+app.use(express.static('public'));    // Be able to serve static files, looks in public folder
+app.use(express.urlencoded({extended: true}));        // Parse data in request body for PUT requests
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
 
 // Set up routes
 app.use('/', mainRoutes);
