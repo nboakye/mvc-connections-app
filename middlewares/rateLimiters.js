b5c467f3b73822d5ef674e9309cf4a8920d545ee
Nanayaw Boakye <nboakye@uncc.edu>
2021-12-08 22:24:51 -0500

    Added rate limiter

---
+++ middlewares/rateLimiters.js
@@ -0,0 +1,12 @@
+const rateLimit = require('express-rate-limit');
+
+express.logInLimiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 5,
+    //message: 'That was too many log in requests. Try again later.'
+    handler: (req, res, next) =>{
+        let err = new Error('That was too many log in requests. Try again later');
+        err.status = 429;
+        return next(err);
+    }
+});
